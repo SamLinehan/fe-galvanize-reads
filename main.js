d10ad766d64ccc80bc47f83e508737d2d97a63d8
Samuel <Linehan@Samuels-MacBook-Air.local>
2016-01-18 20:55:09 -0700

    formatted books and authors lists

--- main.js
+++ main.js
@@ -37,7 +37,31 @@ $(document).ready(function(){
 
 
   $.get('http://localhost:3000/authors', function(data){
-    console.log(data);
+    for(var i = 0; i < data.rows.length; i++){
+      var authormark = document.createElement('div');
+
+      var authorPortrait = document.createElement('img');
+      $(authorPortrait).attr({
+        src: data.rows[i].portrait_url,
+        alt: "Image for " + data.rows[i].first_name + " " + data.rows[i].last_name
+      });
+      $(authorPortrait).css('height', '100px');
+      $(authorPortrait).css('width', '100px');
+      $(authormark).append(authorPortrait);
+
+      var authorName = document.createElement('h3');
+      authorName.innerText = data.rows[i].first_name + " " + data.rows[i].last_name;
+      $(authormark).append(authorName);
+
+      var authorBio = document.createElement('p');
+      authorBio.innerText = data.rows[i].biography;
+      $(authormark).append(authorBio);
+
+
+      $('.authorsListContainer').append(authormark);
+      $(authormark).css('display', 'flex');
+      $(authormark).css('flex-direction', 'row');
+    }
   })
 
   $('.booksList').css('display', 'none');
